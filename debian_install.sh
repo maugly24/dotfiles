7ac1a04664d9bc851bb56ef3e90c4dc1c4a37646
Attila Szabo <maugli@apache.org>
2020-11-12 21:33:55 +0100

    command-not-found support added to debian install

--- debian_install.sh
+++ debian_install.sh
@@ -2,4 +2,7 @@
 
 sudo apt remove --assume-yes vim.tiny
 sudo apt autoremove --assume-yes
-sudo apt install --assume-yes ack bash-completion locate ctags curl git gcc info man mc net-tools p7zip-full progress rsync subversion telnet tig unzip vim vim-doc vim-scripts
+sudo apt install --assume-yes ack bash-completion command-not-found locate ctags curl git gcc info man mc net-tools p7zip-full progress rsync subversion telnet tig unzip vim vim-doc vim-scripts
+sudo apt-file update
+sudo update-command-not-found
+
